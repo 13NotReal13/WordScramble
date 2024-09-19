fd0dbdc54d2e16a36f58b9a3cf19400afad9d458
Ivan Semikin <i.semikin.94@icloud.com>
2024-09-19 19:52:11 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Иван Семикин on 19/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
